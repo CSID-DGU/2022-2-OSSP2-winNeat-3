551ecd4671e3a378a0d9b9fd4c37868a85062846
garden14 <ci667@dgu.ac.kr>
2022-11-30 00:16:09 +0900

    인텐트 데이터 값 수신 코드 수정

--- app/src/main/java/com/example/winneat/MainActivity.kt
+++ app/src/main/java/com/example/winneat/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.winneat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.winneat.databinding.ActivityLoginBinding
+import com.example.winneat.databinding.ActivityMainBinding
+import com.example.winneat.databinding.ActivityRegisterBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(intent.hasExtra("userId")&&intent.hasExtra("userPassword")){
+            val userId = intent.getStringExtra("userId")
+            val userPassword = intent.getStringExtra("userPassword")
+            Log.d("log", "메인에서 받아온 id : $userId, pw : $userPassword")
+        }
+
     }
 }
