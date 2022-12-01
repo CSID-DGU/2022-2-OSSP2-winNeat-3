c9009a48c061ab7f770da6ae7fa3b70990978e09
garden14 <ci667@dgu.ac.kr>
2022-12-01 19:32:16 +0900

    선택한 경기장의 가게를 불러올 interface

---
+++ app/src/main/java/com/example/winneat/APISStore.kt
@@ -0,0 +1,41 @@
+package com.example.winneat
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface APISStore {
+    //post -> php 파일 주소
+    @FormUrlEncoded
+    @POST(MyApp.Store_url)
+    @Headers(
+        "accept: application/json",
+        "content-type: application/x-www-form-urlencoded; charset=utf-8"
+    )
+
+    // Post로 서버에 데이터를 보내는 메서드 - 로그인
+    fun postStadium(
+        // 서버에 Post 방식으로 보낼 떄 사용하는  파라미터의 키 값
+        //ex)@Field('키') =>  $_POST['키']
+        @Field("stadiumName") stadiumName: String,
+    ): Call<storeResult>
+
+    companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
+        //서버 IP만 입력해주세요~
+        private const val BASE_URL = "http://winandeat.dothome.co.kr/"
+        fun create(): APISStore {
+            val gson: Gson = GsonBuilder().setLenient().create();
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(APISStore::class.java)
+        }
+    }
+}
