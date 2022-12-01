21722d45f0ba147cae7e3e79c7827161dd19e8f1
garden14 <ci667@dgu.ac.kr>
2022-12-01 19:33:36 +0900

    가게 정보 data class

---
+++ app/src/main/java/com/example/winneat/PostStore.kt
@@ -0,0 +1,25 @@
+package com.example.winneat
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class storeResult(
+    @Expose
+    @SerializedName("store")
+    var storeList:List<PostStore> = arrayListOf()
+)
+
+data class PostStore(
+    @Expose
+    @SerializedName("storeName")
+    var storeName: String?,
+    @Expose
+    @SerializedName("storeLoc")
+    var storeLoc: String?,
+    @Expose
+    @SerializedName("phoneNum")
+    var phoneNum: Int?,
+    @Expose
+    @SerializedName("stadiumName")
+    var stadiumName: String?,
+)
