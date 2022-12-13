df6e51d1a3d10460c37708614e4a6cdc89b08ba8
garden14 <ci667@dgu.ac.kr>
2022-12-14 03:31:20 +0900

    DB에서 가게 정보 받아오기 - 홍정원

--- app/src/main/java/com/example/winneat/MainActivity.kt
+++ app/src/main/java/com/example/winneat/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.winneat.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    val api by lazy { APISStore.create() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        // setContentView(R.layout.activity_main)
@@ -18,6 +22,34 @@ class MainActivity : AppCompatActivity() {
             val userPassword = intent.getStringExtra("userPassword")
             Log.d("log", "메인에서 받아온 id : $userId, pw : $userPassword")
         }
+
+        val postStadium="광주기아챔피언스필드"
+
+        api.postStadium(postStadium).enqueue(object : Callback<storeResult> {
+            override fun onResponse(call: Call<storeResult>, response: Response<storeResult>) {
+                // PostLogin 클래스 형식으로 php에 데이터 Post, php가 응답한 데이터 또한 PostLogin 클레스 형식으로 받아옴
+//                    Log.d("log",response.toString())
+//                    Log.d("log", response.body().toString())
+                Log.d("log", "성공 : ${response.body()}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
+                val body = response.body()
+                val len = response.body()?.storeList?.size
+                Log.d("log", "len : ${len}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
+
+                //val storeName= response.body()?.storeList?.get(1)?.storeName
+                for(i in 0 until len!!){
+                    Log.d("log", "${i}번째 stadiumName : ${response.body()!!.storeList[i].stadiumName}")
+                    Log.d("log", "${i}번째 storeLoc : ${response.body()!!.storeList[i].storeLoc}")
+                    Log.d("log", "${i}번째 storeName : ${response.body()!!.storeList[i].storeName}")
+                    Log.d("log", "${i}번째 phoneNum : ${response.body()!!.storeList[i].phoneNum}")
+                }
+
+            }
+            override fun onFailure(call: Call<storeResult>, t: Throwable) {
+                Log.d("log","${t.localizedMessage}")
+
+            }
+        })
+
         initBottomNavigation()
     }
 
