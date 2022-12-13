2a3dd2cc30ffda7ce6813e50c845997f20691403
garden14 <ci667@dgu.ac.kr>
2022-12-14 02:57:09 +0900

    MainActivity kt 파일 수정 - 김영채

--- app/src/main/java/com/example/winneat/MainActivity.kt
+++ app/src/main/java/com/example/winneat/MainActivity.kt
@@ -18,6 +18,38 @@ class MainActivity : AppCompatActivity() {
             val userPassword = intent.getStringExtra("userPassword")
             Log.d("log", "메인에서 받아온 id : $userId, pw : $userPassword")
         }
+        initBottomNavigation()
+    }
 
+    private fun initBottomNavigation() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.lockerFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LockerFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
