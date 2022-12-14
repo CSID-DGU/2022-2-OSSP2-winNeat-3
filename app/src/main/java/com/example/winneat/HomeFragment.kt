29b0de37dbfe4688d89ba14f91b11bf5f7319a3b
garden14 <ci667@dgu.ac.kr>
2022-12-14 20:43:31 +0900

    PostData 폴더 생성으로 인한 경로 변경 - 홍정원

--- app/src/main/java/com/example/winneat/HomeFragment.kt
+++ app/src/main/java/com/example/winneat/HomeFragment.kt
@@ -12,6 +12,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.winneat.PostData.PostStore
 import com.example.winneat.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
