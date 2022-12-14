6b78ca59260918de9785b98bcf1613ba49850edb
garden14 <ci667@dgu.ac.kr>
2022-12-14 21:10:42 +0900

    경기장 선택 액티비티 생성 - 전우진

---
+++ app/src/main/java/com/example/winneat/StadiumSelectActivity.kt
@@ -0,0 +1,11 @@
+package com.example.winneat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StadiumSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stadium_select)
+    }
+}
