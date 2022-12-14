e540c329e3687055e607e7c4ad70d746a51b4019
garden14 <ci667@dgu.ac.kr>
2022-12-14 16:45:53 +0900

    main에서 전송되어 온 데이터 처리 - 홍정원

--- app/src/main/java/com/example/winneat/HomeFragment.kt
+++ app/src/main/java/com/example/winneat/HomeFragment.kt
@@ -1,14 +1,23 @@
 package com.example.winneat
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.winneat.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     lateinit var binding: FragmentHomeBinding
+   // var storeList= mutableListOf<PostStore>()
+    lateinit var recyclerView: RecyclerView
+    var storelist: ArrayList<PostStore>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +26,59 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
+        val qlist = arguments?.getSerializable("storeList")
+         storelist = qlist as? ArrayList<PostStore>
+
+        Log.d("fragment 들어옴", qlist.toString())
+        Log.d("fragment 들어옴", storelist.toString())
+
+
+        recyclerView = binding.rvResult
+        recyclerView.adapter = ResultRecyclerViewAdpater()
+        recyclerView.layoutManager = LinearLayoutManager(activity?.getApplicationContext())
+
         return binding.root
+
+    }
+
+    inner class ResultRecyclerViewAdpater() :
+        RecyclerView.Adapter<ResultRecyclerViewAdpater.ResultViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ResultRecyclerViewAdpater.ResultViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.store_item, parent, false)
+            return ResultViewHolder(view)
+        }
+
+        override fun getItemCount(): Int {
+            return storelist!!.size
+        }
+
+        override fun onBindViewHolder(
+            holder: ResultRecyclerViewAdpater.ResultViewHolder,
+            position: Int
+        ) {
+            holder.apply {
+                nameTextView.text = storelist?.get(position)?.storeName
+                if(storelist?.get(position)?.storeCategory.equals("떡볶이")){
+                    Glide.with(itemView).load("https://cdn.pixabay.com/photo/2016/08/20/13/06/toppokki-1607479_960_720.jpg").override(400,200).centerCrop().into(storePic)
+                }else if (storelist?.get(position)?.storeCategory.equals("호프")){
+                    Glide.with(itemView).load("https://cdn.pixabay.com/photo/2017/02/25/15/23/barbecue-2098020_960_720.jpg").override(400,200).centerCrop().into(storePic)
+                }else if (storelist?.get(position)?.storeCategory.equals("치킨")){
+                    Glide.with(itemView).load("http://www.bhc.co.kr/upload/bhc/menu/ckc20150130_470_v.jpg").override(400,200).centerCrop().into(storePic)
+                }else if (storelist?.get(position)?.storeCategory.equals("햄버거")){
+                    Glide.with(itemView).load("https://cdn.pixabay.com/photo/2014/10/19/20/59/hamburger-494706_960_720.jpg").override(400,200).centerCrop().into(storePic)
+                }
+
+            }
+        }
+
+        inner class ResultViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+            //위의 onCreateViewHolder에서 생성된 view를 가지고 실행함
+            val nameTextView: TextView = view.findViewById(R.id.store_name)
+            val storePic : ImageView = view.findViewById(R.id.storePic)
+
+        }
     }
 }
