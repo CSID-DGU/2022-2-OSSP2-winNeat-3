29f2da48e9bc8fc986d3d63b8c1c8faa52729ad4
garden14 <ci667@dgu.ac.kr>
2022-12-14 21:10:23 +0900

    경기장 액티비티 초기 작성, 클릭이벤트 구현 - 전우진

--- app/src/main/java/com/example/winneat/StadiumActivity.kt
+++ app/src/main/java/com/example/winneat/StadiumActivity.kt
@@ -1,11 +1,22 @@
 package com.example.winneat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.winneat.databinding.ActivityMainBinding
+import com.example.winneat.databinding.ActivityStadiumBinding
 
 class StadiumActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStadiumBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_stadium)
+        //setContentView(R.layout.activity_stadium)
+
+        binding.stadiumBtn.setOnClickListener{ //스타디움 텍스트 눌렀을 때
+            val intent = Intent(this, StadiumSelectActivity::class.java)
+            startActivity(intent) // 경기장 선택 화면 이동
+        }
+
     }
 }
