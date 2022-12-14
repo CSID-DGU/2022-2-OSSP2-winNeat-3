59ace60ac6b9c66cb240df5e002ccf483e929b4a
garden14 <ci667@dgu.ac.kr>
2022-12-14 16:45:22 +0900

    main에서 home fragment 데이터 전송 코드 수정 - 홍정원

--- app/src/main/java/com/example/winneat/MainActivity.kt
+++ app/src/main/java/com/example/winneat/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.winneat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.winneat.APIS.APISStore
 import com.example.winneat.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,24 +28,45 @@ class MainActivity : AppCompatActivity() {
         }
 
         val postStadium="광주기아챔피언스필드"
-
+        val argBundle = Bundle()
+        var argList = arrayListOf<PostStore>()
+        //var argList = mutableListOf<PostStore>()
         api.postStadium(postStadium).enqueue(object : Callback<storeResult> {
             override fun onResponse(call: Call<storeResult>, response: Response<storeResult>) {
                 // PostLogin 클래스 형식으로 php에 데이터 Post, php가 응답한 데이터 또한 PostLogin 클레스 형식으로 받아옴
 //                    Log.d("log",response.toString())
 //                    Log.d("log", response.body().toString())
-                Log.d("log", "성공 : ${response.body()}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
+//                Log.d("log", "성공 : ${response.body()}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
                 val body = response.body()
                 val len = response.body()?.storeList?.size
-                Log.d("log", "len : ${len}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
+
+//                Log.d("log", "len : ${len}") // php가 보내온 아이디, 비밀번호가 로그캣에 띄워짐
+
+                val list : List<PostStore>? = response.body()?.storeList
+                argList= response.body()?.storeList as ArrayList<PostStore>
+                Log.d("storelist", list.toString())
+
+                val bundle = Bundle()
+                bundle.putSerializable("storeList",(list as Serializable))
+                argBundle.putSerializable("storeList",(list as Serializable))
+
+                val homeFragment = HomeFragment()
+                homeFragment.arguments = bundle
+
+                val manager:FragmentManager=supportFragmentManager
+                val transaction:FragmentTransaction=manager.beginTransaction()
+                transaction.replace(R.id.main_frm,homeFragment).commit()
+
+                Log.d("bundle 확인", bundle.toString())
+
 
                 //val storeName= response.body()?.storeList?.get(1)?.storeName
-                for(i in 0 until len!!){
-                    Log.d("log", "${i}번째 stadiumName : ${response.body()!!.storeList[i].stadiumName}")
-                    Log.d("log", "${i}번째 storeLoc : ${response.body()!!.storeList[i].storeLoc}")
-                    Log.d("log", "${i}번째 storeName : ${response.body()!!.storeList[i].storeName}")
-                    Log.d("log", "${i}번째 phoneNum : ${response.body()!!.storeList[i].phoneNum}")
-                }
+//                for(i in 0 until len!!){
+//                    Log.d("log", "${i}번째 stadiumName : ${response.body()!!.storeList[i].stadiumName}")
+//                    Log.d("log", "${i}번째 storeLoc : ${response.body()!!.storeList[i].storeLoc}")
+//                    Log.d("log", "${i}번째 storeName : ${response.body()!!.storeList[i].storeName}")
+//                    Log.d("log", "${i}번째 phoneNum : ${response.body()!!.storeList[i].phoneNum}")
+//                }
 
             }
             override fun onFailure(call: Call<storeResult>, t: Throwable) {
@@ -50,7 +75,32 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
-        initBottomNavigation()
+      //  initBottomNavigation()
+
+        binding.mainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    argBundle.putSerializable("storeList",(argList))
+                    argBundle.putSerializable("storeSize",argList.size)
+                  //  argBundle.putString(argList.toString())
+
+                    val homeFragment = HomeFragment()
+                    homeFragment.arguments = argBundle
+
+                    val manager:FragmentManager=supportFragmentManager
+                    val transaction:FragmentTransaction=manager.beginTransaction()
+                    transaction.replace(R.id.main_frm,homeFragment).commit()
+
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_frm, HomeFragment())
+//                        .commitAllowingStateLoss()
+
+                    return@setOnItemSelectedListener true
+                }
+
+        }
+            false
+        }
     }
 
     private fun initBottomNavigation() {
@@ -61,12 +111,12 @@ class MainActivity : AppCompatActivity() {
         binding.mainBnv.setOnItemSelectedListener{ item ->
             when (item.itemId) {
 
-                R.id.homeFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
+//                R.id.homeFragment -> {
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_frm, HomeFragment())
+//                        .commitAllowingStateLoss()
+//                    return@setOnItemSelectedListener true
+//                }
 
                 R.id.searchFragment -> {
                     supportFragmentManager.beginTransaction()
