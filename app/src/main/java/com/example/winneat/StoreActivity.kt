31bb9e680912a09a2b7522bedd318ad14ef88db7
garden14 <ci667@dgu.ac.kr>
2022-12-15 15:23:27 +0900

    php를 통해 데이터 가져오는 코드 추가 - 홍정원

--- app/src/main/java/com/example/winneat/StoreActivity.kt
+++ app/src/main/java/com/example/winneat/StoreActivity.kt
@@ -3,12 +3,22 @@ package com.example.winneat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.viewpager2.widget.ViewPager2
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.setFragmentResultListener
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.winneat.APIS.APISMenu
+import com.example.winneat.PostData.PostMenu
+import com.example.winneat.PostData.menuResult
 import com.example.winneat.databinding.ActivityStoreBinding
-import com.google.android.material.tabs.TabLayoutMediator
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.Serializable
 
 class StoreActivity : AppCompatActivity() {
     lateinit var binding: ActivityStoreBinding
+    val api by lazy { APISMenu.create() }
 
     var userId = ""
     var userPassword = ""
@@ -22,47 +32,107 @@ class StoreActivity : AppCompatActivity() {
 
 //        setContentView(R.layout.activity_store)
 
-
         if(intent.hasExtra("userId")&&intent.hasExtra("userPassword")&&intent.hasExtra("storeName")){
             userId = intent.getStringExtra("userId").toString()
             userPassword = intent.getStringExtra("userPassword").toString()
             storeName = intent.getStringExtra("storeName").toString()
             stadiumName = intent.getStringExtra("stadiumName").toString()
 
-            Log.d("log", "받아온 id : $userId, pw : $userPassword, storeName : $storeName, stadiumName : $stadiumName")
-
-
+          //  Log.d("log", "받아온 id : $userId, pw : $userPassword, storeName : $storeName, stadiumName : $stadiumName")
         }
-        initViewPager()
+        binding.storeNameText.text=storeName
 
-    }
+        var argList = arrayListOf<PostMenu>()
 
-    private fun initViewPager() {
-        //ViewPager2 Adapter 셋팅
-        var viewPager2Adatper = ViewPager2Adapter(this)
-        viewPager2Adatper.addFragment(MenuFragment())
-        viewPager2Adatper.addFragment(R_Info_Fragment())
-        viewPager2Adatper.addFragment(ReviewFragment())
+        api.postMenu(stadiumName, storeName).enqueue(object : Callback<menuResult> {
+            override fun onResponse(call: Call<menuResult>, response: Response<menuResult>) {
 
-        //Adapter 연결
-        binding.vpViewpagerMain.apply {
-            adapter = viewPager2Adatper
+                val body = response.body()
+                Log.d("log","${body}")
 
-            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                }
-            })
-        }
+                val list : List<PostMenu>? = response.body()?.MenuList
+                argList= response.body()?.MenuList as ArrayList<PostMenu>
 
-        //ViewPager, TabLayout 연결
-        TabLayoutMediator(binding.tlNavigationView, binding.vpViewpagerMain) { tab, position ->
-            Log.e("YMC", "ViewPager position: ${position}")
-            when (position) {
-                0 -> tab.text = "메뉴"
-                1 -> tab.text = "정보"
-                2 -> tab.text = "리뷰"
+                val bundle = Bundle()
+                bundle.putSerializable("menuList",(list as Serializable))
+                bundle.putString("userId",userId)
+                bundle.putString("userPassword",userPassword)
+                bundle.putString("storeName",storeName)
+                bundle.putString("stadiumName",stadiumName)
+
+                val menuFragment = MenuFragment()
+                menuFragment.arguments = bundle
+                Log.d("log", "$bundle")
+
+
+                val manager:FragmentManager=supportFragmentManager
+                val transaction:FragmentTransaction=manager.beginTransaction()
+                transaction.replace(R.id.store_main_frm,menuFragment).commit()
+
             }
-        }.attach()
+            override fun onFailure(call: Call<menuResult>, t: Throwable) {
+                Log.d("log","${t.localizedMessage}")
+
+            }
+        })
+
+
+        binding.storeMainBnv.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.menuFragment -> {
+                    val bundle = Bundle()
+                    bundle.putString("userId",userId)
+                    bundle.putString("userPassword",userPassword)
+                    bundle.putString("storeName",storeName)
+                    bundle.putString("stadiumName",stadiumName)
+
+                    val menuFragment = MenuFragment()
+                    menuFragment.arguments = bundle
+                    Log.d("log", "$bundle")
+
+                    val manager:FragmentManager=supportFragmentManager
+                    val transaction:FragmentTransaction=manager.beginTransaction()
+                    transaction.replace(R.id.store_main_frm,menuFragment).commit()
+
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.main_frm, HomeFragment())
+//                        .commitAllowingStateLoss()
+
+                    return@setOnItemSelectedListener true
+                }
+                R.id.Fragment_R_Info -> {
+                    val bundle = Bundle()
+                    bundle.putString("userId",userId)
+                    bundle.putString("userPassword",userPassword)
+                    bundle.putString("storeName",storeName)
+                    bundle.putString("stadiumName",stadiumName)
+
+                    val RInfoFragment = R_Info_Fragment()
+                    RInfoFragment.arguments = bundle
+                    Log.d("STORE TO RINFO", "$bundle")
+
+                    val manager:FragmentManager=supportFragmentManager
+                    val transaction:FragmentTransaction=manager.beginTransaction()
+                    transaction.replace(R.id.store_main_frm,RInfoFragment).commit()
+
+//                    supportFragmentManager.beginTransaction()
+//                        .replace(R.id.store_main_frm, R_Info_Fragment())
+//                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.ReviewFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.store_main_frm, ReviewFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+
+        }
+
     }
+
 }
+
+
